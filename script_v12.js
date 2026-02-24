851badbff5585a85a8c4efc3ddea817101853f12
cxymwo <w2vz9wr4rk@privaterelay.appleid.com>
2026-02-24 18:17:05 +0100

    clock it

--- script_v12.js
+++ script_v12.js
@@ -1,9 +1,7 @@
 function showMenu() {
-    document.getElementById('dropdown').style.opacity = '1';
-    document.getElementById('dropdown').style.transform = 'translateY(0px)';
+  document.getElementById('dropdown', 'dropdownX').style.opacity = '1';
 }
 
 function hideMenu() {
-    document.getElementById('dropdown').style.opacity = '0';
-    document.getElementById('dropdown').style.transform = 'translateY(-60px)';
+  document.getElementById('dropdown', 'dropdownX').style.opacity = '0';
 }
