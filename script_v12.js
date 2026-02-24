27d2cf6b9021a856f734c3342cfa1b79cdf4c2d9
cxymwo <w2vz9wr4rk@privaterelay.appleid.com>
2026-02-24 13:20:55 +0100

    drop down done, animation okay

---
+++ script_v12.js
@@ -0,0 +1,7 @@
+function showMenu() {
+  document.getElementById('dropdown', 'dropdownX').style.display = 'block';
+}
+
+function hideMenu() {
+  document.getElementById('dropdown', 'dropdownX').style.display = 'none';
+}
