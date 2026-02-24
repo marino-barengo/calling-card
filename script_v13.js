9fc1ed97889b57b4d718bc56a2a77da92e2ff324
cxymwo <w2vz9wr4rk@privaterelay.appleid.com>
2026-02-24 17:48:53 +0100

    v14_NOCH MEHR GOATED

---
+++ script_v13.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    document.getElementById('dropdown').style.opacity = '1';
+    document.getElementById('dropdown').style.transform = 'translateY(0px)';
+}
+
+function hideMenu() {
+    document.getElementById('dropdown').style.opacity = '0';
+    document.getElementById('dropdown').style.transform = 'translateY(-60px)';
+}
